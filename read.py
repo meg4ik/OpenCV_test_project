20a53e93934c5ece0f63131fd6d1a4e173ebafc1
PLOheMfW <meg4mag@gmail.com>
2021-06-04 15:13:53 +0300

    Update read.py

--- read.py
+++ read.py
@@ -2,9 +2,23 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
+def changeRes(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+#changeRes(700, 400)
+
 while True:
     _, frame = cap.read()
-    cv.imshow('Cam 0', frame)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #blur = cv.GaussianBlur(frame, (1,1), cv.BORDER_DEFAULT)
+    canny = cv.Canny(gray, 100, 100)
+    #text = cv.putText(canny, "Cam 0", (520,25), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 0,0),2)
+
+    conturs, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    
+
+    cv.imshow('Cam 0', canny)
     if cv.waitKey(1) == ord('q'):
         break
 cap.release()
