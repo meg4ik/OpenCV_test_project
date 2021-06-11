f0ddb42cb7b01bacd26a3b844ef90aa13b577604
PLOheMfW <meg4mag@gmail.com>
2021-06-11 20:48:00 +0300

    --

--- read.py
+++ read.py
@@ -6,19 +6,21 @@ def changeRes(width, height):
     cap.set(3, width)
     cap.set(4, height)
 
-#changeRes(700, 400)
+changeRes(1000, 600)
+
+haar_casc = cv.CascadeClassifier('haar_fullbody.xml')
 
 while True:
     _, frame = cap.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #blur = cv.GaussianBlur(frame, (1,1), cv.BORDER_DEFAULT)
-    canny = cv.Canny(gray, 100, 100)
-    #text = cv.putText(canny, "Cam 0", (520,25), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 0,0),2)
 
-    conturs, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    faces = haar_casc.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=1)
     
+    cv.imshow('Cam 0', frame)
 
-    cv.imshow('Cam 0', canny)
     if cv.waitKey(1) == ord('q'):
         break
 cap.release()
